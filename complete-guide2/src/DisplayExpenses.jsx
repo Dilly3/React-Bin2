f8e203443c9ea3844abaebbf25762de8859ff2dc
Anikammadu Michael <dil.anikamadu@gmail.com>
2022-06-06 16:17:36 +0100

    display expenses component

---
+++ complete-guide2/src/DisplayExpenses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DisplayExpenses.css'
+
+export function DisplayExpenses(props){
+
+    return <>
+    <div >
+        {props.amount}
+    </div>
+    
+    </>
+}
